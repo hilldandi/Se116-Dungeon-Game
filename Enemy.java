36b17c1358a660c137adda491f0a3e7bbb50c819
FEzgiERKAT <105327029+FEzgiERKAT@users.noreply.github.com>
2022-05-27 23:44:27 +0300

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,7 +1,15 @@
+package Others;
+
 public class Enemy extends Character {
-
     private int strength,intelligence,vitality;
-    public Enemy(){
-
+    private double hP;
+    public Enemy() {
+        super.setIntelligence(GameLogic.rand.nextInt(5) + 1);
+        super.setStrength(GameLogic.rand.nextInt(5) + 1);
+        super.setVitality(GameLogic.rand.nextInt(5) + 1);
+        //hP=0.7*vitality+0.2*strength+0.1*intelligence;
+        //hp yazdırt burada
+        //eğer herhangi bir ekstra özellik tanımlamak stersen soonradan örnek gibi yap
+        //this.seatHeight=seatHeight;
     }
-}
+    }
