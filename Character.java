25ef0f174de696ed6c411314462274a89faf92c3
FEzgiERKAT <105327029+FEzgiERKAT@users.noreply.github.com>
2022-05-27 23:41:29 +0300

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,45 +1,84 @@
+package Others;
+
 public class Character {
-    int type = 0;
 
-
-
-    double hP=0.0;
-    int strength,vitality,intelligence;
+    int type;
+    private int strength,vitality,intelligence;
+    private double hP;
+    private double inventoryWeight;
     boolean isWeaponWielded=false;
+
     public int getType() {
         return type;
     }
 
-    void attack(Character character){//action requires a weapon to be wielded. It also requires a specific target.characters can attack other characters too
-        if(!isWeaponWielded){
-            System.out.println("You need to wield a weapon to attack");
-        }
-        else{
-            character.hP-=strength;
-        }
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getVitality() {
+        return vitality;
+    }
+
+    public void setVitality(int vitality) {
+        this.vitality = vitality;
+    }
+
+    public void sethP(int vitality,int  strength,int intelligence) {
+        this.hP=hP;
+    }
+    public double gethP() {
+        return hP;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public void attack() {//action requires a weapon to be wielded. It also requires a specific target.characters can attack other characters too
+        //if(!isWeaponWielded){
+          //  System.out.println("You need to wield a weapon to attack");
+        //}
+        //else{
+          //  character.hP-=strength;
+        //}
+    }
+    public void specialAction(){//to use the special action of their weapon requires a specific target, including the character itself
 
     }
-    void specialAction(){//to use the special action of their weapon requires a specific target, including the character itself
+    public void pick(){//picks up an item on the ground Requires a specific target. The item is stored in the character's inventory
 
     }
-    void pick(){//picks up an item on the ground Requires a specific target. The item is stored in the character's inventory
+    public void wield(){//wield a weapon from its inventory or from the ground. Requires a specific targe
 
     }
-    void wield(){//wield a weapon from its inventory or from the ground. Requires a specific targe
+    public void wear(){//wear armor from its inventory or from the ground
 
     }
-    void wear(){//wear armor from its inventory or from the ground
+    public void examine(){ //examine an item to learn its attributes
 
     }
-    void examine(){ //examine an item to learn its attributes
-
-
-    }
-    void carryInventory(){
+    public void carryInventory(){
 
     }
-    void listInventory(){
+    public void listInventory(){
 
     }
-    public void round(){}
+    public void round(){
+
+    }
+
+
 }
