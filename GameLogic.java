2ad99f5b12742e0a561fe8acf862c8244f3c83a0
FEzgiERKAT <105327029+FEzgiERKAT@users.noreply.github.com>
2022-05-27 23:43:48 +0300

    Update GameLogic.java

--- GameLogic.java
+++ GameLogic.java
@@ -1,3 +1,5 @@
+package Others;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
@@ -6,44 +8,15 @@ public class GameLogic {
     static Scanner input=new Scanner(System.in);
     static Random rand =new Random();
     static ArrayList<Character> characters=new ArrayList<Character>(3);
-
-    public static Character chooseCharacter(String input){
-        Character character=null;
-        String[] characterArray = input.split(" ");
-        for (int i = 0; i <characters.size() ; i++) {
-            Character c = characters.get(i);
-            if(c.getType()==1 && characterArray[0].equalsIgnoreCase("fighter")){
-                character=c;
-            }
-            else if(c.getType()==2 && characterArray[0].equalsIgnoreCase("healer")){
-                character=c;
-            }
-            else if(c.getType()==3 && characterArray[0].equalsIgnoreCase("tank")){
-                character=c;
-            }
-
-
-        }
-        return character;
-    }
-    public static String takeAction(String input){
-        String[] splitted =input.split(" ");
-        if(splitted.length!=3){
-            return null ;
-        }
-        return splitted[1];
+    //String[] characterArray = input.split(" ");
+    public void menuBeginning() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter yourname : ");
+        String playerN = input.next();
     }
 
-    public static String takeTarget(String input){
-        String[] splitted =input.split(" ");
-        if(splitted.length!=3){
-            return null ;
-        }
-        return splitted[2];
-    }
-
-
-
+    //*public boolean stopOrCont(){
+        //if ()}
     //item part will be here
 
 
