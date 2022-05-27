cfaa0316ffaa156e87daeb3b448cf66408d4fc23
FEzgiERKAT <105327029+FEzgiERKAT@users.noreply.github.com>
2022-05-27 23:44:55 +0300

    Update Healer.java

--- Healer.java
+++ Healer.java
@@ -1,17 +1,16 @@
+package Others;
+
 public class Healer extends Character {
-
-
-
-    int  type = 2 ;
-
     private int strength,intelligence,vitality;
-
+    private double hP;
     public Healer() {
-
-    }
-
-    @Override
-    public int getType() {
-        return type;
+        super.setIntelligence(GameLogic.rand.nextInt(5)+1);
+        super.setStrength(GameLogic.rand.nextInt(5)+3);
+        super.setVitality(GameLogic.rand.nextInt(5)+6);
+        //hP=0.7*vitality+0.2*strength+0.1*intelligence;
+        //hp yazdırt burada
+        //eğer herhangi bir ekstra özellik tanımlamak stersen soonradan örnek gibi yap
+        //this.seatHeight=seatHeight;
+        System.out.println("Healer generated. Strength:" + getStrength()+ " Vitality:"+ getVitality() + " Intelligence:" +getIntelligence()+ " HP:" + gethP());
     }
 }
