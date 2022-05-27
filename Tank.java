ffe3d2c60ea48e74e0895d66d60abba4bdd5b4da
FEzgiERKAT <105327029+FEzgiERKAT@users.noreply.github.com>
2022-05-27 23:46:05 +0300

    Update Tank.java

--- Tank.java
+++ Tank.java
@@ -1,16 +1,16 @@
+package Others;
+
 public class Tank extends Character {
-
-    int  type = 3 ;
-
     private int strength,intelligence,vitality;
-
+    private double hP;
     public Tank() {
-
-
-    }
-
-    @Override
-    public int getType() {
-        return type;
+        super.setIntelligence(GameLogic.rand.nextInt(5)+3);
+        super.setStrength(GameLogic.rand.nextInt(5)+1);
+        super.setVitality(GameLogic.rand.nextInt(5)+6);
+        //hP=0.7*vitality+0.2*strength+0.1*intelligence;
+        //hp yazdırt burada
+        //eğer herhangi bir ekstra özellik tanımlamak stersen soonradan örnek gibi yap
+        //this.seatHeight=seatHeight;
+        System.out.println("Tank generated. Strength:" + getStrength()+ " Vitality:"+ getVitality() + " Intelligence:" +getIntelligence()+ " HP:" + gethP());
     }
 }
